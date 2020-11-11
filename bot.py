32a9fb9118d2907313eb9b9b5d7cf68d76128dd6
sw08 <63825580+sw08@users.noreply.github.com>
2020-11-11 15:47:53 +0900

    업뎃

--- bot.py
+++ bot.py
@@ -377,7 +377,7 @@ async def _botinfo(ctx):
     msgembed.add_field(name='개발자', value='Team ThinkingBot')
     msgembed.add_field(name='도움을 주신 분들', value='`huntingbear21#4317`님, `Decave#9999`님, `koder_ko#8504`님, `Scott7777#5575`님, `Minibox#3332`님 등 많은 분들께 감사드립니다.', inline=False)
     msgembed.add_field (name='상세정보', value='다른 봇에서는 볼 수 없는 독특한 기능들이 많이 있음', inline=False)
-    msgembed.add_field(name='버전', value='1.3.3 - 20201111 릴리즈', inline=False)
+    msgembed.add_field(name='버전', value='1.3.4 - 20201111 릴리즈', inline=False)
     msgembed.add_field(name='개발언어 및 라이브러리', value='파이썬, discord.py', inline=False)
     msgembed.add_field(name='링크', value='[깃허브 바로가기](https://github.com/sw08/thinkingbot)\n[봇 초대 링크](https://discord.com/api/oauth2/authorize?client_id=750557247842549871&permissions=0&scope=bot)\n[공식 서포트 서버](https://discord.gg/ASvgRjX)\n[공식 홈페이지](http://thinkingbot.kro.kr)', inline=False)
     msgembed.set_thumbnail(url="https://sw08.github.io/cloud/profile.png")
